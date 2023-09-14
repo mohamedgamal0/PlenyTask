3eb81ffc6de1af9c6a4d957ce04ea413624ba23c
Mohamed Gamal <mohamed.gamal@areebgroup.com>
2023-09-14 15:53:47 +0300

    Initial Commit

---
+++ PlenyTask/PlenyTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlenyTaskApp.swift
+//  PlenyTask
+//
+//  Created by Mohamed Gamal on 14/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlenyTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
